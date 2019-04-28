4b3fdb679e3b0a4399473bd808f20528fa4fd13b
Arshpreet Singh <arsh840@gmail.com>
2019-04-29 02:44:41 +0530

    adding hyper table for timescale

--- insert_data_timescale.py
+++ insert_data_timescale.py
@@ -1,34 +1,16 @@
-'''
-CREATE database tutorial;
-Lastly add TimescaleDB:
-
--- Connect to the database
-\c tutorial
-
--- Extend the database with TimescaleDB
-CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
-
-connection_string = "user=test dbname=test password=test host=0.0.0.0 sslmode=disable port=5000"
-'''
-
-
-#select bounce_reason,bounce_class_name,classification_id,bounce_type,bounce_class_desc,account_id,count(*) from bounceautodata123 WHERE time='2019-03-18 11:32:22.987938+00' GROUP BY bounce_reason,bounce_class_name,classification_id,bounce_type,bounce_class_desc,account_id;
-
-# ceate your DB acording to this one as well:
-#{"account_id":"","from":"jj@jj.com","rcpt":"jj@jj.com","domain":"jj.com","status":"bounced","bounce_type":"soft","diagnostic":"[internal] Domain Lookup Failed.","retry":1,"ip":"127.0.0.1"}
-
-
 #!/usr/bin/python
 
 import psycopg2
+from datetime import datetime
+import time
+import random
 
 # connecting throug docke r as well
 conn = psycopg2.connect(database = "relay", user = "maropost", password = "Maro123!", host = "0.0.0.0", port = "5432")
 print ("Opened database successfully")
 cur = conn.cursor()
 
-#cur.execute("CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;")
-
+query_timescale = """CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"""
 
 schema_create_table = """
 CREATE TABLE bouncefinaldata (
@@ -51,21 +33,15 @@ STATUS			TEXT		NOT NULL
 );
 
 """
-from datetime import datetime
-import time
-import random
-#cur.execute(schema_create_table);
-#conn.commit()
-#time.sleep(2)
 
+create_hypertable = """SELECT create_hypertable('bouncefinaldata','time','bounce_reason',4);"""
+
+cur.execute(schema_create_table);
+conn.commit()
 print ("table created success fully")
+cur.execute(create_hypertable);
+print ("Hyper table created success fully")
 
-import time
-data_insert_string = """
-
-"""
-
-read_data_query = """SELECT * from bouncestest3"""
 
 random_list_email = ["3636@maorpost.com","346@maorpost.com","36@maorpost.com","346@maorpost.com","3463@maorpost.com","346@maorpost.com","346@maorpost.com","3453@maorpost.com","34@maorpost.com","3245@maorpost.com","324345@maorpost.com","23@maorpost.com"]
 random_list_domain = ["345435.com","435634.biz","43645.net","45654.co.in","4565436.hello"]
@@ -74,9 +50,7 @@ bounce_reason = ["email not exist","blocked attachment","mailbox full","message 
 bounce_class_type = ["bounce class one","bounce class two","bounce class three"]
 bounce_code = [200,400,500,800]
 
-# now based on bounce code I have to update the column name indside the things as well.
 
-#rows = cur.fetchall()
 for i in range(10,1000):
     timestamp = time.time()
     dt_object = datetime.fromtimestamp(timestamp)
@@ -84,27 +58,4 @@ for i in range(10,1000):
     random.choice(random_list_domain),float(i)*2.0,float(i)*4.0,str(i),random.choice(bounce_type),random.choice(bounce_class_type),str(i),i*11,i*22,i*123,random.choice(bounce_reason),str(i)))
     print ("doing things here as well")
     time.sleep(2)
-    #print ("inserting data here as well")
-    #print ("this is new string we need to look for")	
-    #print (new_string)
-    #cur.execute(new_string)
     conn.commit()
-    #print ("Reading data here as well")
-    #cur.execute(read_data_query)
-    #rows = cur.fetchall()
-    #print ("these are the rows we need to look ")
-    #print rows
-
-'''
-cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (2, 'Allen', 25, 'Texas', 15000.00 )");
-
-cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )");
-
-cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
-'''
-#conn.commit()
-#print ("Records created successfully")
-#conn.close()
