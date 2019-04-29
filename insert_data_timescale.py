a067445fff8cca3a94fe04fd3845a64e839bd1f1
Arshpreet Singh <arshpreet.singh@maropost.com>
2019-04-29 20:08:43 +0530

    changes in timescale

--- insert_data_timescale.py
+++ insert_data_timescale.py
@@ -13,32 +13,32 @@ cur = conn.cursor()
 query_timescale = """CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"""
 
 schema_create_table = """
-CREATE TABLE bounces123456 (
+CREATE TABLE bounces123 (
 time 		TIMESTAMP		NOT NULL,
-account_id	CHAR			NOT NULL,
-sub_account_id	CHAR			NOT NULL,
-to_email	CHAR			NOT NULL,
-from_email	CHAR			NOT NULL,
-from_domain	CHAR			NOT NULL, 
-injection_ip	CHAR	NOT NULL,
-remote_ip	CHAR	NOT NULL,
-bounce_type	CHAR			NOT NULL,
-bounce_class_name	CHAR		NOT NULL,
-bounce_class_desc       CHAR            NOT NULL,
-classification_id	CHAR		NOT NULL,
-transmission_id		CHAR		NOT NULL,
-message_id		CHAR		NOT NULL,
-bounce_reason		CHAR		NOT NULL, 
-STATUS			CHAR		NOT NULL 
+account_id	TEXT			NOT NULL,
+sub_account_id	TEXT			NOT NULL,
+to_email	TEXT			NOT NULL,
+from_email	TEXT			NOT NULL,
+from_domain	TEXT			NOT NULL, 
+injection_ip	TEXT	NOT NULL,
+remote_ip	TEXT	NOT NULL,
+bounce_type	TEXT			NOT NULL,
+bounce_class_name	TEXT		NOT NULL,
+bounce_class_desc       TEXT            NOT NULL,
+classification_id	TEXT		NOT NULL,
+transmission_id		TEXT		NOT NULL,
+message_id		TEXT		NOT NULL,
+bounce_reason		TEXT		NOT NULL, 
+STATUS			TEXT		NOT NULL 
 );
 
 """
 
 create_hypertable = """SELECT create_hypertable('bounces123456','time','bounce_reason',4);"""
 
-#cur.execute(schema_create_table);
-#conn.commit()
-#print ("table created success fully")
+cur.execute(schema_create_table);
+conn.commit()
+print ("table created success fully")
 #cur.execute(create_hypertable);
 #print ("Hyper table created success fully")
 
@@ -54,7 +54,7 @@ bounce_code = [200,400,500,800]
 for i in range(1000000):
     timestamp = time.time()
     dt_object = datetime.fromtimestamp(timestamp)
-    cur.execute("INSERT INTO bouncefinaldata VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(dt_object,str(123*i),str(1234*i),random.choice(random_list_email),
+    cur.execute("INSERT INTO bounces123 VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(dt_object,str(123*i),str(1234*i),random.choice(random_list_email),
     random.choice(random_list_domain),str(i*2.0),str(i*4.0),str(i),random.choice(bounce_type),random.choice(bounce_class_type),str(i),str(i*11),str(i*22),str(i*123),random.choice(bounce_reason),str(i)))
     print ("doing things here as well")
     conn.commit()
